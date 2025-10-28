ce4c53395d693890e384655b71aae2e67f53a1ea
Ammar Khan <ammarkhanpathan421@gmail.com>
2025-10-28 13:30:31 +0530

    Create Practical3a_Car.java
    use default constructor

---
+++ Practical3a_Car.java
@@ -0,0 +1,8 @@
+class Practical3a_Car {
+    Practical3a_Car() { 
+        System.out.println("Car object created");
+    }
+    public static void main(String[] args) {
+        Practical3a_Car myCar = new Practical3a_Car();
+    }
+}
